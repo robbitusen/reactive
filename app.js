48f418cea3c34e5609fb9e3dc5b4bc1bc78f9bec
Jake <jake@shopifybuilder.com>
2016-05-22 19:38:50 +0100

    BLUE

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ function init() {
 	createSea();
 	createSky();
 
+	//Add Listeners
+	document.addEventListener('mousemove', handleMouseMove, false);
+
 	//Start a loop that will update the object's positions and render the scene for each frame
 	loop();
 
-}
+};
 
 function createScene(){
 
@@ -66,7 +69,41 @@ function createScene(){
 
 var hemisphereLight, shadowLight;
 
-function createLights(){};
+function createLights(){
+
+	// A hemisphere light is a gradient colored light; 
+	// the first parameter is the sky color, the second parameter is the ground color, 
+	// the third parameter is the intensity of the light
+	hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, 0.9);
+
+	// A directional light shines from a specific direction. 
+	// It acts like the sun, that means that all the rays produced are parallel.
+	shadowLight = new THREE.DirectionalLight(0xffffff, 0.9);
+
+	// Set the direction of the light  
+	shadowLight.position.set(150, 350, 350);
+
+	//Allow shadow casting
+	shadowLight.castShadow = true;
+
+	//Define the visible area of the projected shadow 
+	shadowLight.shadow.camera.left = -400;
+	shadowLight.shadow.camera.right = 400;
+	shadowLight.shadow.camera.top = 400;
+	shadowLight.shadow.camera.bottom = -400;
+	shadowLight.shadow.camera.near = 1;
+	shadowLight.shadow.camera.far = 1000;
+
+	// define the resolution of the shadow; the higher the better, 
+	// but also the more expensive and less performant
+	shadowLight.shadow.mapSize.width = 2048;
+	shadowLight.shadow.mapSize.height = 2048;
+
+	//To activate the lights, just add them to the scene
+	scene.add(hemisphereLight);
+	scene.add(shadowLight);
+
+};
 
 function createPlane(){};
 
@@ -94,7 +131,7 @@ var Sea = function() {
 	//Allow sea to recieve shadows
 	this.mesh.recieveShadow = true;
 
-}
+};
 
 var sea;
 
@@ -118,6 +155,11 @@ function handleWindowResize(){
 	renderer.setSize(WIDTH, HEIGHT);
 	camera.aspect = WIDTH / HEIGHT;
 	camera.updateProjectionMatrix();
+};
+
+var mousePos = {x: 0, y: 0};
+function handleMouseMove(){
+
 };
 
 function loop(){
